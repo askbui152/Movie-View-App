76249c1acc38881d5f32fa20f555caaa26dbc468
James Bui <jamesbui@vt.edu>
2016-01-06 12:37:09 -0500

    Part 3 Complete

--- Movie View App/MoviesViewController.swift
+++ Movie View App/MoviesViewController.swift
@@ -50,7 +50,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movies = movies {
+        if let movies = movies {    // in case our network is not working 
             return movies.count
         }
         else {
@@ -59,10 +59,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
-        cell.textLabel!.text = title
+        let overview = movie["overview"] as! String 
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
         print("row \(indexPath.row)")
         return cell
     }
