680845a901b25d079dbd8e32f3235a58a624e0ee
James Bui <jamesbui@vt.edu>
2016-01-06 11:21:00 -0500

    Initial Commit

---
+++ Movie View App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie View App
+//
+//  Created by James Bui on 1/6/16.
+//  Copyright © 2016 James Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
